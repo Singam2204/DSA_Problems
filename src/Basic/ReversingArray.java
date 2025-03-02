83b5deaa0471b7ed2325c7da1857fd6c4cd530a1
Your Name <snehasingam2003@gmail.com>
2025-03-03 00:42:47 +0530

    Added Reversing Array

---
+++ src/Basic/ReversingArray.java
@@ -0,0 +1,35 @@
+package Basic;
+import java.util.Scanner;
+public class ReversingArray{
+    public static void main(String[] args) {
+        System.out.println("Enter length of array");
+        Scanner n = new Scanner(System.in);
+        int len = n.nextInt();
+        int[] arr = new int[len];
+        int[] arr2 = new int[len];
+        System.out.println("Enter values");
+        for (int i = 0; i < len; i++) {
+            arr[i]=n.nextInt();
+        }
+        for(int i=0; i < len;i++){
+            System.out.print(arr[i]+" ");
+        }
+        for(int j=len-1; j>=0;j--){
+            arr2[len-1-j]=arr[j];
+        }
+//        for(int j=0;j<len-1;j++){
+//            arr2[j]= arr[len-1-j];
+//        }
+
+//        int k=0;
+//        for(int j=arr.length-1; j>=0;j--){
+//            arr2[k]=arr[j];
+//            k++;
+//        }
+        System.out.println();
+        System.out.print("Array 2: ");
+        for(int i=0; i < len;i++){
+            System.out.print(arr2[i]+" ");
+        }
+        }
+}
